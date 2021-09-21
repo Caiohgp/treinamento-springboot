43179bece5aabb859a300f1821e88b8e988d6f11
Caiohgp <caio.102008@gmail.com>
2021-09-21 17:02:21 -0300

    Added repository and service package

--- src/main/java/projeto/projetospringboot2/controller/AnimeController.java
+++ src/main/java/projeto/projetospringboot2/controller/AnimeController.java
@@ -1,6 +1,10 @@
 package projeto.projetospringboot2.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import projeto.projetospringboot2.domain.Anime;
+import projeto.projetospringboot2.service.AnimeService;
 import projeto.projetospringboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -12,15 +16,21 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
 	private final DateUtil dateUtil;
+	private final AnimeService animeService;
 
-	@GetMapping(path = "list")
-	public List<Anime> list(){
+	@GetMapping
+	public ResponseEntity<List<Anime>> list(){
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return List.of(new Anime("One Piece"),new Anime("Berserk"));
+		return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Anime> findById(@PathVariable long id){
+		return ResponseEntity.ok(animeService.findById(id));
 	}
 }
---
+++ src/main/java/projeto/projetospringboot2/repository/AnimeRepository.java
@@ -0,0 +1,9 @@
+package projeto.projetospringboot2.repository;
+
+import projeto.projetospringboot2.domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+    List<Anime> listAll();
+}

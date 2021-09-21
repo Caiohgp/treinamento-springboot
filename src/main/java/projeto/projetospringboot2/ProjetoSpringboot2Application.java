ea89feda8cf5c05f79326c0bd898fc275d44c0cb
Caiohgp <caio.102008@gmail.com>
2021-09-21 11:52:07 -0300

    First commit

---
+++ src/main/java/projeto/projetospringboot2/ProjetoSpringboot2Application.java
@@ -0,0 +1,13 @@
+package projeto.projetospringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringboot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/projetospringboot2/ProjetoSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.projetospringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

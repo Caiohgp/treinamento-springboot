17bf07c49624c8d7f1d303f45df7581d960015c1
Caiohgp <caio.102008@gmail.com>
2021-09-22 17:29:59 -0300

    Added POST Method

--- src/main/java/projeto/projetospringboot2/service/AnimeService.java
+++ src/main/java/projeto/projetospringboot2/service/AnimeService.java
@@ -5,20 +5,33 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import projeto.projetospringboot2.domain.Anime;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L,"One Piece"),new Anime(2L,"Berserk"));
+    private static List<Anime> animes;
+
+    static{
+        animes = new ArrayList<>(List.of(new Anime(1L,"One Piece"),new Anime(2L,"Berserk")));
+    }
 
     public List<Anime> listAll(){
  		return animes;
     }
+
     public Anime findById(long id){
         return animes.stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime Not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,1000000));
+        animes.add(anime);
+        return anime;
     }
 }

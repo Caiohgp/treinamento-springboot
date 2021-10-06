2bed69241c29229deb1cbc3468d443aefe8dc3f2
Caio <caio.102008@gmail.com>
2021-10-05 21:55:21 -0300

    Added field validation on the post request

--- src/main/java/projeto/projetospringboot2/requests/AnimePostRequestBody.java
+++ src/main/java/projeto/projetospringboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,11 @@ package projeto.projetospringboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message="The anime cannot be empty")
     private String name;
 }

d065570268e89b214e8f77f00a0e8ebc86d38781
Caiohgp <caio.102008@gmail.com>
2021-10-06 16:23:06 -0300

    Added field validation via handler

--- src/main/java/projeto/projetospringboot2/handler/RestExceptionHandler.java
+++ src/main/java/projeto/projetospringboot2/handler/RestExceptionHandler.java
@@ -2,12 +2,17 @@ package projeto.projetospringboot2.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import projeto.projetospringboot2.exception.BadRequestException;
 import projeto.projetospringboot2.exception.BadRequestExceptionDetails;
+import projeto.projetospringboot2.exception.ValidationExceptionDetails;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -20,6 +25,24 @@ public class RestExceptionHandler {
                         .title("Bad Request Exception, check the Documentation")
                         .details(bre.getMessage())
                         .developerMessage(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Invalid Fields")
+                        .details(fieldsMessage)
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/projeto/projetospringboot2/requests/AnimePostRequestBody.java
+++ src/main/java/projeto/projetospringboot2/requests/AnimePostRequestBody.java
@@ -1,6 +1,7 @@
 package projeto.projetospringboot2.requests;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -9,4 +10,6 @@ import javax.validation.constraints.NotNull;
 public class AnimePostRequestBody {
     @NotEmpty(message="The anime cannot be empty")
     private String name;
+    @URL(message = "The URL is not valid")
+    private String url;
 }

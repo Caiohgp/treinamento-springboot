f79b02d4ca0819354b9059718ce916e0af34c806
Caiohgp <caio.102008@gmail.com>
2021-10-19 16:28:41 -0300

    Rest Template Post method

--- src/main/java/projeto/projetospringboot2/client/SpringClient.java
+++ src/main/java/projeto/projetospringboot2/client/SpringClient.java
@@ -2,8 +2,7 @@ package projeto.projetospringboot2.client;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import projeto.projetospringboot2.domain.Anime;
 
@@ -22,10 +21,25 @@ public class SpringClient {
         Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all",Anime[].class);
         log.info(Arrays.toString(animes));
 
-        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET,null,
-        new ParameterizedTypeReference<List<Anime>>() {});
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Anime>>() {});
         log.info(exchange.getBody());
 
 
+
+        Anime samurai = Anime.builder().name("Samurai").build();
+        ResponseEntity<Anime> samuraiSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.POST,
+                new HttpEntity<>(samurai, createJsonHeader()),
+                Anime.class);
+        log.info("Saved Anime {}",samuraiSaved);
+    }
+
+    private static HttpHeaders createJsonHeader(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
